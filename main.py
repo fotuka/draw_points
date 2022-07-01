5d74ae15e5bbef7729009983c453e794481d60eb
fotuka <antda08@yandex.ru>
2022-07-01 14:39:40 +0300

    def __init__ for Grind and edit lenght name in GrindSlope

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 
 class Grid:
 
+    def __init__(self, length, height, xline_amount, yline_amount):
+        self.length = length
+        self.height = height
+        self.xline_amount = xline_amount
+        self.yline_amount = yline_amount
+        self.xy = np.zeros([self.xline_amount * self.yline_amount, 2], float)
+
     def create(self):
-        length = int_input_variable("Введите длину ")
-        height = int_input_variable("Введите высоту ")
-        xline_amount = int_input_variable("Введите кол-во вертикальных перекладин ")
-        yline_amount = int_input_variable("Введите кол-во горизонтальных перекладин ")
-        self.xy = np.zeros([xline_amount * yline_amount, 2], float)
-        xgap = length / xline_amount  # расстояние между перекладинами по вертикали
-        ygap = height / yline_amount  # расстояние между перекладинами по горизонтали
+        xgap = self.length / self.xline_amount  # расстояние между перекладинами по вертикали
+        ygap = self.height / self.yline_amount  # расстояние между перекладинами по горизонтали
         index = 0
-        for y in np.arange(0, height, ygap):
-            for x in np.arange(0, length, xgap):
+        for y in np.arange(0, self.height, ygap):
+            for x in np.arange(0, self.length, xgap):
                 self.xy[index, 0] = x
                 self.xy[index, 1] = y
                 index += 1
@@ -23,21 +25,21 @@ class Grid:
 
 class GridSlope:
 
-    def __init__(self, lenght, height, xline_amount, yline_amount):
-        self.lenght = lenght
+    def __init__(self, length, height, xline_amount, yline_amount):
+        self.length = length
         self.height = height
         self.xline_amount = xline_amount
         self.yline_amount = yline_amount
         self.xy = np.zeros([self.xline_amount * self.yline_amount, 2], float)
 
     def create(self):
-        xgap = self.lenght / (self.xline_amount - 1)  # расстояние между перекладинами по вертикали
+        xgap = self.length / (self.xline_amount - 1)  # расстояние между перекладинами по вертикали
         ygap = self.height / (self.yline_amount - 1)  # расстояние между перекладинами по горизонтали
         k = 0
         index = 0
         for y in np.arange(0, self.height + ygap, ygap):
             k += 1
-            for x in np.arange(0, self.lenght + ygap, xgap):
+            for x in np.arange(0, self.length + ygap, xgap):
                 self.xy[index, 1] = y
                 if k % 2 == 0:
                     self.xy[index, 0] = x + xgap * 0.5
