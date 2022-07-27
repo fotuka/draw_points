61279e375581b931b5609289e6b7e1db4fa74dad
fotuka <antda08@yandex.ru>
2022-07-27 14:07:32 +0300

    add type hints and annotations

--- calculation.py
+++ calculation.py
@@ -6,47 +6,51 @@ FULL_ANGLE = 360
 
 
 class Coordinates:
-    def __init__(self):
+    def __init__(self) -> 'Initializing array':
         self.xy = np.array([])
 
-    def create(self):
+    def create(self) -> 'Creating an array of points':
         pass
 
-    def rotate(self, degree):
+    def rotate(self, degree: 'Angle of rotation (float)') -> 'Rotated array':
         for index in range(len(self.xy)):
             self.xy[index, 0] = (self.xy[index, 0] * math.cos(math.radians(degree))) - (self.xy[index, 1] * math.sin(math.radians(degree)))
             self.xy[index, 1] = (self.xy[index, 0] * math.sin(math.radians(degree))) + (self.xy[index, 1] * math.cos(math.radians(degree)))
         return self.xy
 
-    def move_x(self, x):
+    def move_x(self, x: 'Distance (float)') -> 'Moved array along x':
         for index in range(len(self.xy)):
             self.xy[index, 0] = self.xy[index, 0] + x
         return self.xy
 
-    def move_y(self, y):
+    def move_y(self, y: 'Distance (float)') -> 'Moved array along y':
         for index in range(len(self.xy)):
             self.xy[index, 1] = self.xy[index, 1] + y
         return self.xy
 
-    def export(self, path):
+    def export(self, path: 'Path to folder (str)') -> 'Saves array to folder':
         np.savetxt(path, self.xy)
         self.xy.tofile('path', sep=' ')
 
 
 class Rectangle(Coordinates):
-    def __init__(self, length, height, xline_amount, yline_amount):
+    def __init__(self,
+                 length: 'Length of rectangle (float)',
+                 height: 'Height of rectangle (float)',
+                 xline_amount: 'Amount of horizontal lines (int)',
+                 yline_amount: 'Amount of vertical lines (int)') -> 'Initializing an empty rectangle array of points':
         super().__init__()
         self.length = length
         self.height = height
         self.xline_amount = xline_amount
         self.yline_amount = yline_amount
         self.xy = np.zeros([self.xline_amount * self.yline_amount, 2], float)
-        self.xgap = self.length / self.xline_amount  # расстояние между перекладинами по вертикали
-        self.ygap = self.height / self.yline_amount  # расстояние между перекладинами по горизонтали
+        self.xgap = self.length / self.xline_amount  # gap between vertical lines
+        self.ygap = self.height / self.yline_amount  # gap between horizontal lines
 
 
 class Grid(Rectangle):
-    def create(self):
+    def create(self) -> 'Creating an array of points':
         index = 0
         for y in np.arange(0, self.height, self.ygap):
             for x in np.arange(0, self.length, self.xgap):
@@ -56,9 +60,9 @@ class Grid(Rectangle):
 
 
 class GridSlope(Rectangle):
-    def create(self):
-        self.xgap = self.length / (self.xline_amount - 1)  # расстояние между перекладинами по вертикали
-        self.ygap = self.height / (self.yline_amount - 1)  # расстояние между перекладинами по горизонтали
+    def create(self) -> 'Creating an array of points':
+        self.xgap = self.length / (self.xline_amount - 1)  # gap between vertical lines
+        self.ygap = self.height / (self.yline_amount - 1)  # gap between horizontal lines
         k = 0
         index = 0
         for y in np.arange(0, self.height + self.ygap, self.ygap):
@@ -73,7 +77,10 @@ class GridSlope(Rectangle):
 
 
 class Circle(Coordinates):
-    def __init__(self, radius, dots_amount, lines_amount):
+    def __init__(self,
+                 radius: ' Radius of circle (float)',
+                 dots_amount: 'Amount of dots on single line (int)',
+                 lines_amount: 'Amount of lines (int)') -> 'Initializing an empty circle array of points':
         super().__init__()
         self.radius = radius
         self.dots_amount = dots_amount
@@ -84,7 +91,7 @@ class Circle(Coordinates):
 
 
 class Snow(Circle):
-    def create(self):
+    def create(self) -> 'Creating an array of points':
         index = 1
         for line in range(0, self.lines_amount):
             for value in np.arange(self.gap, self.radius + self.gap, self.gap):
@@ -94,7 +101,7 @@ class Snow(Circle):
 
 
 class SnowAdvanced(Circle):
-    def create(self):
+    def create(self) -> 'Creating an array of points':
         index = 1
         for line in range(0, self.lines_amount):
             if line % 2 == 0:
