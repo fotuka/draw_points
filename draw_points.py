8eace5a0af077dc7a0564d783b64b47f029bc87f
fotuka <antda08@yandex.ru>
2022-07-28 01:56:21 +0300

    'none' to None and write readme

--- draw_points.py
+++ draw_points.py
@@ -69,7 +69,7 @@ class DrawPoints:
         self.actions = []
         self.menu = self.tr(u'&Draw Points')
         self.dlg = DrawPointsDialog()
-        self.choose = 'none'
+        self.choose = None
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
